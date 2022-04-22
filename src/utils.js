17207a8eaa4412ef9bfe5b042451bfe0b1172e1c
Your Name <your@example.com>
2022-04-22 10:55:18 +1000

    Add server and style css

--- src/utils.js
+++ src/utils.js
@@ -8,4 +8,4 @@ const add = (a,b) => a+b;
 const subtract = (a,b) => a - b;
 //exports - default export - named exports
 
-export { square, add, subtract as default};
+export {square, add, subtract as default};
